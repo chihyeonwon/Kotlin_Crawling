cfe55ec10f4373b353b44c3a83abbee2397ae913
ylose <heungeob1003@gmail.com>
2023-10-30 20:06:49 +0900

    메뉴아이템 데이터 클래스

---
+++ app/src/main/java/com/example/wonjumenu/MenuItem.kt
@@ -0,0 +1,8 @@
+package com.example.wonjumenu
+
+// 학식 객체 저장할 데이터 클래스
+data class MenuItem (
+    val breakfast : String,     // 조식
+    val lunch : String,       // 중식
+    val dinner : String,      // 석식
+)
